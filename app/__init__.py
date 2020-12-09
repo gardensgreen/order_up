038867b9f61fea388146f3cc2efabef13caa9faf
Corbin Armendariz <corbinarmendariz@gmail.com>
2020-12-09 16:17:10 -0600

    for the boys

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,12 @@ from .config import Configuration
 from .models import db, Employee
 from .routes import orders
 from flask_login import LoginManager
-
+from .routes import session
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 app.register_blueprint(orders.bp)
+app.register_blueprint(session.bp)
 db.init_app(app)
 
 
