e02bffcae0a428d63af13baf7715a53a4163b34f
Corbin Armendariz <corbinarmendariz@gmail.com>
2020-12-09 15:51:21 -0600

    for the garden

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,20 @@
 from flask import Flask
 from .config import Configuration
-from .models import db   # New import
+from .models import db, Employee
 from .routes import orders
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 app.register_blueprint(orders.bp)
 db.init_app(app)
+
+
+login = LoginManager(app)
+login.login_view = "session.login"
+
+
+@login.user_loader
+def load_user(id):
+    return Employee.query.get(int(id))
--- app/models.py
+++ app/models.py
@@ -1,3 +1,26 @@
+from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
+
+
+class Employee(db.Model, UserMixin):
+  __tablename__= "employees"
+  id = db.Column(db.Integer, primary_key = True)
+  name = db.Column(db.String(100), nullable = False )
+  employee_number = db.Column(db.Integer, nullable = False, unique = True)
+  hashed_password = db.Column(db.String(100), nullable = False)
+
+  @property
+  def password(self):
+      return self.hashed_password
+
+
+  @password.setter
+  def password(self, password):
+      self.hashed_password = generate_password_hash(password)
+
+
+  def check_password(self, password):
+      return check_password_hash(self.password, password)
